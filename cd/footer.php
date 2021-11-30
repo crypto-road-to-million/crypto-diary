4d3994d97ec51623c9b877c2b50048b13e055b42
paagr <mason.paolo@gmail.com>
2021-11-30 19:35:58 +0100

    fix footer bug link

--- cd/footer.php
+++ cd/footer.php
@@ -2,7 +2,7 @@
   <div class="container">
     <p><?php esc_html_e( 'Copyright &copy; ', 'cd' ); ?><?php echo esc_url( home_url( ) ); ?> <?php echo date('o'); ?>
     </p>
-    <?php if(get_field('github_issue_url')) { ?>
+    <?php if(get_field('github_issue_url', 'option')) { ?>
     <p>
       <a class="text-decoration-none link-light fw-bold"
         href="<?php the_field('github_issue_url', 'option'); ?>"><?php esc_html_e( 'Submit Feature Request or Bug', 'cd' ); ?></a>
